68b1ae296a2e7124125c12ff2452e489e54a3a8d
BenShelton <bensheltonjones@gmail.com>
2021-09-30 13:51:00 +0100

    test(index): add tests to read test directories and check reports are correct

---
+++ tests/fixtures/src-clean/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p v-text="message" />
+    <Other />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Other from './Other.vue'
+import { message } from './messages'
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {
+    Other
+  },
+
+  computed: {
+    message (): string {
+      return message
+    }
+  }
+})
+</script>
